a7bec2c71c554336ea26ff71c92ece4396b7b172
BeTrai <betrai@BeTrais-MacBook-Pro.local>
2020-10-15 15:59:51 +0700

    Update home_lang.php

--- application/language/vietnamese/home_lang.php
+++ application/language/vietnamese/home_lang.php
@@ -4,4 +4,5 @@ $lang['OUR <span>PRODUCTS</span>'] = "Sản <span>phẩm</span>";
 $lang['OUR <span>SERVICES</span>']	= "Dịch <span>vụ</span>";
 
 $lang['view more'] = 'Xem chi tiết';
+$lang['view detail'] = "xem chi tiết";
 ?>
