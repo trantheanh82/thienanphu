e052a0bf716c49b49e6cf03759b0c883facc9e7b
BeTrai <betrai@BeTrais-MacBook-Pro.local>
2020-10-15 17:46:19 +0700

    Update sliders.php

--- application/modules/sliders/controllers/sliders.php
+++ application/modules/sliders/controllers/sliders.php
@@ -2,15 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Sliders extends Public_Controller {
-	
+
 	function __construct(){
 		parent::__construct();
 		$this->load->model('slider_model');
-		
+
 	}
-	
+
 	function sliders(){
-		$this->data['sliders'] = $this->slider_model->get_all();
+		$this->data['sliders'] = $this->slider_model->where('active','Y')->get_all();
 		$this->render('sliders/sliders','module');
 	}
 }
