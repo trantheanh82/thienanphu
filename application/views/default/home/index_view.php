d4ea8161a175b583f749de12da1e54974424d0b7
BeTrai <betrai@BeTrais-MacBook-Pro.local>
2020-10-15 14:22:00 +0700

    Update index_view.php

--- application/views/default/home/index_view.php
+++ application/views/default/home/index_view.php
@@ -1,18 +1,18 @@
 <?php
-	
-	echo Modules::run('sliders/sliders/sliders');
-	
+
+	echo Modules::run('sliders/sliders');
+
 	$this->load->view('elements/modules/home_about',array('home_about'=>$home_about));
-	
+
 	$this->load->view('elements/modules/home_parallax',array('background_image'=>'blink-edited.jpg'));
-	
+
 	$this->load->view('elements/modules/home_solutions',array('home_solutions'=>$home_solutions));
-	
+
 	$this->load->view('elements/modules/home_parallax',array('background_image'=>'blink-edited.jpg'));
-	
+
 	$this->load->view('elements/modules/home_services',array('home_services'=>$home_services));
-	
+
 	$this->load->view('elements/modules/home_funfact',array('funcfact'=>""));
-	
+
 	$this->load->view('elements/modules/home_news',array('home_news'=>$home_article));
 ?>
