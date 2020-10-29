7cd8742b0c7c7fd2a53485d154f6295a4c9ea8ec
trantheanh82 <raijuichi@live.com>
2020-10-29 16:45:52 +0700

    Update solutions_detail.php

--- application/views/default/solutions/solutions_detail.php
+++ application/views/default/solutions/solutions_detail.php
@@ -2,11 +2,13 @@
 <section id="course_all" class="padding-bottom-half padding-top">
   <div class="container">
     <div class="row">
+      <div class='col-lg-12 col-md-12'>
+
 	    <div class="course_detail wow fadeIn animated animated" data-wow-delay="400ms" style="visibility: visible; animation-delay: 400ms; animation-name: fadeIn;">
         <?=img($item->image,false, array('alt'=>$item->name,'class'=>'border_radius img-responsive bottom15'));?>
-        
+
         <div class="detail_course">
-          
+
           <!--<div class="info_label">
             <span class="icony"><i class="icon-users3"></i></span>
             <p>Tư vấn</p>
@@ -28,24 +30,25 @@
                 <input type="radio" name="star" class="star-3" id="star-03">
                 <label class="star-3" for="star-03">3</label>
                 <input type="radio" name="star" class="star-4" id="star-04" checked="">
-                <label class="star-4" for="star-04">4</label> 
+                <label class="star-4" for="star-04">4</label>
                 <input type="radio" name="star" class="star-5" id="star-05">
                 <label class="star-5" for="star-05">5</label>
                 <span></span>
               </div>
             </form>
-            
+
           </div>-->
         </div>
         <h3 class="top30 bottom20"><?=$item->name?></h3>
-        
+
         <div class='service-content'>
         <?=$item->content?>
         </div>
         <div class="bottom15"></div>
         <?=$this->load->view('elements/modules/module_share_social')?>
-        
+
       </div>
+    </div>
     </div>
   </div>
 </section>
