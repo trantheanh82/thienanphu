5ffd98112f444fb7f0103936be53733a15f331ca
trantheanh82 <raijuichi@live.com>
2020-11-30 17:06:32 +0700

    update
    abc

--- application/views/elements/modules/home_parallax.php
+++ application/views/elements/modules/home_parallax.php
@@ -10,7 +10,7 @@
       <div class="col-md-12 text-center wow bounceIn">
        <p class="bottom25 whitecolor">Nâng cao khả năng quản lý với Thiên An Phú</p>
        <h1 class="bottom25 whitecolor">Tìm kiếm giải pháp CNTT?</h1>
-       <a href="#." class="border_radius btn_common white_border">Gọi để tư vấn</a>
+       <a href="tel:<?=$Settings['company_phone_1']?>" class="border_radius btn_common white_border">Gọi để tư vấn</a>
       </div>
     </div>
   </div>
--- application/views/elements/modules/module_hotline.php
+++ application/views/elements/modules/module_hotline.php
@@ -1,5 +1,5 @@
 <div class="widget heading_space">
-	<h3 class="bottom20">HOTINE TƯ VẤN</h3>
+	<!--<h3 class="bottom20">HOTINE TƯ VẤN</h3>-->
 	<div class="media">
     	<a class="media-left" href="tel:0913717000">
 	    	<?=img('assets/img/hotline-tuvan.jpg',false,array('width'=>'100%','alt'=>'post'))?>
