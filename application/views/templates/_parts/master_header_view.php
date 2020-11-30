7b6cf69e70819a6ffa53c4384113d9ea0cb64958
trantheanh82 <raijuichi@live.com>
2020-11-30 16:18:20 +0700

    Update master_header_view.php

--- application/views/templates/_parts/master_header_view.php
+++ application/views/templates/_parts/master_header_view.php
@@ -45,7 +45,7 @@
 	<meta name='keywords' content='".$keywords."' />
 	<meta name='description' content='".$description."' />";
 
-	$url =  "http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+	$url =  "https://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
 
 	$escaped_url = htmlspecialchars( $url, ENT_QUOTES, 'UTF-8' );
 	echo "
