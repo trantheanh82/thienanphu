1820b6c5c0c5a7cc5ceadb46ec32b7f997cae4f4
trantheanh82 <raijuichi@live.com>
2020-12-03 15:35:07 +0700

    Update login_view.php

--- application/views/admin/login_view.php
+++ application/views/admin/login_view.php
@@ -8,15 +8,15 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <?php echo link_tag('assets/admin/bootstrap/dist/css/bootstrap.min.css')?>
+  <?php echo link_tag('/assets/admin/bootstrap/dist/css/bootstrap.min.css')?>
   <!-- Font Awesome -->
-    <?php echo link_tag('assets/admin/font-awesome/css/font-awesome.min.css')?>
+    <?php echo link_tag('/assets/admin/font-awesome/css/font-awesome.min.css')?>
   <!-- Ionicons -->
-  <?php echo link_tag('assets/admin/Ionicons/css/ionicons.min.css')?>
+  <?php echo link_tag('/assets/admin/Ionicons/css/ionicons.min.css')?>
   <!-- Theme style -->
-  <?php echo link_tag('assets/admin/css/AdminLTE.min.css')?>
+  <?php echo link_tag('/assets/admin/css/AdminLTE.min.css')?>
   <!-- iCheck -->
-  <?php echo link_tag('assets/admin/plugins/iCheck/square/blue.css')?>
+  <?php echo link_tag('/assets/admin/plugins/iCheck/square/blue.css')?>
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
